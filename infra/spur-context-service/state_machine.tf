cb3f558b9af6dd658bfd44f3a442280612cd53d9
Truong Cong Hoan Vu <kevin.truong.ds@gmail.com>
2026-06-30 12:02:47 +0700

    feat(context-service): S4-5 add source fetch split infra

--- infra/spur-context-service/state_machine.tf
+++ infra/spur-context-service/state_machine.tf
@@ -10,18 +10,20 @@ resource "aws_sfn_state_machine" "index_build" {
   definition = templatefile(
     "${path.module}/index_build_asl.json",
     {
-      cluster_arn                    = aws_ecs_cluster.indexing.arn
-      worker_taskdef_arn             = aws_ecs_task_definition.worker.arn
-      worker_lambda_arn              = aws_lambda_alias.worker_live.arn
-      worker_lambda_timeout_sec      = var.worker_lambda_timeout_sec
-      worker_ecs_timeout_sec         = var.context_max_build_seconds + 900
-      index_jobs_table_name          = aws_dynamodb_table.index_jobs.name
-      catalog_leases_table_name      = aws_dynamodb_table.catalog_leases.name
-      catalog_dsn                    = local.aurora_catalog_dsn
-      context_ducklake_data_path     = local.context_ducklake_data_path
-      worker_checkpoint_uri_template = local.worker_checkpoint_uri_template
-      subnets_json                   = jsonencode(var.worker_subnets)
-      security_groups_json           = jsonencode([aws_security_group.worker.id])
+      cluster_arn                       = aws_ecs_cluster.indexing.arn
+      worker_taskdef_arn                = aws_ecs_task_definition.worker.arn
+      worker_lambda_arn                 = aws_lambda_alias.worker_live.arn
+      source_fetch_lambda_arn           = aws_lambda_alias.source_fetcher_live.arn
+      worker_lambda_timeout_sec         = var.worker_lambda_timeout_sec
+      source_fetcher_lambda_timeout_sec = var.source_fetcher_lambda_timeout_sec
+      worker_ecs_timeout_sec            = var.context_max_build_seconds + 900
+      index_jobs_table_name             = aws_dynamodb_table.index_jobs.name
+      catalog_leases_table_name         = aws_dynamodb_table.catalog_leases.name
+      catalog_dsn                       = local.aurora_catalog_dsn
+      context_ducklake_data_path        = local.context_ducklake_data_path
+      worker_checkpoint_uri_template    = local.worker_checkpoint_uri_template
+      subnets_json                      = jsonencode(var.worker_subnets)
+      security_groups_json              = jsonencode([aws_security_group.worker.id])
     }
   )
 }
